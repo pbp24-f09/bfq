6df66c143a82a2bed1cd629201596b76175b49ed
danferdiansyah <danferdianstyle@gmail.com>
2024-10-06 17:01:21 +0700

    feat: added model and urls routing to main app

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    restaurant = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    contact = models.IntegerField()
+    
+    @property
+    def is_price_valid(self):
+        return self.price > 0
--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_main(request):
+    context = {
+        'app_name' : 'Bandung Food Quest',
+        'tagline' : "Delicious bandung's food",
+    }
+
+    return render(request, "main.html", context)
