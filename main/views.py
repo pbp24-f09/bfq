7d731dd16c380075d918878127d27c0a28c24051
danferdiansyah <danferdianstyle@gmail.com>
2024-10-07 00:19:00 +0700

    feat: added function to view xml and json (and by its id)

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from main.forms import FoodForm
 from main.models import Food
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_main(request):
     foods = Food.objects.all()
@@ -21,3 +23,19 @@ def create_food(request):
 
     context = {'form': form}
     return render(request, "create_food.html", context)
+
+def show_xml(request):
+    data = Food.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Food.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Food.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Food.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
