d7d4c5dd0d37f18ffd999383c54767160f34a489
danferdiansyah <danferdianstyle@gmail.com>
2024-10-07 00:15:53 +0700

    feat: added create_food form

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, create_food
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('create-food', create_food, name='create_food'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import FoodForm
+from main.models import Food
 
 def show_main(request):
+    foods = Food.objects.all()
+    
     context = {
         'app_name' : 'Bandung Food Quest',
         'tagline' : "Delicious bandung's food",
     }
 
     return render(request, "main.html", context)
+
+def create_food(request):
+    form = FoodForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_food.html", context)
