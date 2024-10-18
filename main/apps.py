0632156265ffaa4140d35f13337f0a0635d29fe2
danferdiansyah <danferdianstyle@gmail.com>
2024-10-19 02:11:43 +0700

    feat: configuring AppConfig to automatically import the json data

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-
+from django.core.management import call_command
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+    def ready(self):
+        call_command('loaddata', 'main/fixtures/products.json')
