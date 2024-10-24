74c0377b415da1ad4eec9b5bc9f5a41cab6555f0
EvelynDepthios <depthiosevelyn98@gmail.com>
2024-10-24 23:14:51 +0700

    Added Fitur Change Password in Authentication App

--- authentication/urls.py
+++ authentication/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('update-profile/', views.update_profile, name='update_profile'),
     path('update-photo/', views.update_photo, name='update_photo'),
     path('delete-photo/', views.delete_photo, name='delete_photo'),
-    # path('change-password/', views.change_password, name='change_password'),
+    path('change-password/', views.change_password, name='change_password'),
 ]
