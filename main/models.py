b30e920247a392b15987085bc82de566b2911455
danferdiansyah <danferdianstyle@gmail.com>
2024-10-24 23:42:38 +0700

    fix: integrating user model to products, allowing to display json database

--- main/models.py
+++ main/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+
 import uuid
 
 class Product(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
     CATEGORY_CHOICES = [
         ('BERAT', 'Makanan Berat dan Nasi'),
         ('OLAHAN', 'Olahan Ayam dan Daging'),
         ('MIE', 'Mie, Pasta, dan Spaghetti'),
         ('SNACK', 'Makanan Ringan dan Cemilan'),
     ]
-
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
