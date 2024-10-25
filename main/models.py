9aa7b20cd0affe3a7d28d1f099b717fd8caa2c68
danferdiansyah <danferdianstyle@gmail.com>
2024-10-25 23:13:05 +0700

    feat: modified category choices model

--- main/models.py
+++ main/models.py
@@ -7,10 +7,10 @@ import uuid
 class Product(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
     CATEGORY_CHOICES = [
-        ('BERAT', 'Makanan Berat dan Nasi'),
-        ('OLAHAN', 'Olahan Ayam dan Daging'),
-        ('MIE', 'Mie, Pasta, dan Spaghetti'),
-        ('SNACK', 'Makanan Ringan dan Cemilan'),
+        ('Makanan Berat dan Nasi', 'Makanan Berat dan Nasi'),
+        ('Olahan Ayam dan Daging', 'Olahan Ayam dan Daging'),
+        ('Mie, Pasta, dan Spaghetti', 'Mie, Pasta, dan Spaghetti'),
+        ('Makanan Ringan dan Cemilan', 'Makanan Ringan dan Cemilan'),
     ]
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
